45535a0a12395d216e2231cd0bce262128460fcb
apurbanand <apurb@unitedpro.in>
2023-10-10 20:04:18 +0530

    Initial commit

--- demo.py
+++ demo.py
@@ -1,58 +1,130 @@
 import streamlit as st
+from st_login_form import login_form
 
-import st_login_form
+# Call set_page_config as the first Streamlit command
+st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
 
-VERSION = st_login_form.__version__
+client = login_form()
 
-st.set_page_config(
-    page_title="Streamlit Login Form",
-    page_icon="🔐",
-    menu_items={
-        "About": f"Streamlit Login Form 🔐 v{VERSION}  "
-        f"\nApp contact: [Siddhant Sadangi](mailto:siddhant.sadangi@gmail.com)",
-        "Report a Bug": "https://github.com/SiddhantSadangi/st-login-form/issues/new",
-        "Get help": None,
-    },
-)
+# Import necessary libraries and functions
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
+from PyPDF2 import PdfReader
+from dotenv import load_dotenv
 
-# ---------- SIDEBAR ----------
-with open("assets/sidebar.html", "r", encoding="UTF-8") as sidebar_file:
-    sidebar_html = sidebar_file.read().replace("{VERSION}", VERSION)
+# Define functions for chat with multiple PDFs
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
-with st.sidebar:
-    st.components.v1.html(sidebar_html, height=600)
 
-# ---------- MAIN PAGE ----------
-st.title("🔐 [`st-login-form`](https://github.com/SiddhantSadangi/st_login_form) demo app ")
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
-st.write(
-    "This app shows how you can use the `st-login-form` component to create user-login forms for Streamlit apps."
-)
 
-st.write("1. Install")
-st.code("pip install st-login-form", language="bash")
-st.write("2. Import")
-st.code("from st_login_form import login_form", language="python")
-st.write("3. Use")
-st.code("client = login_form()", language="python")
-st.info("💡 Explore the arguments you can pass to `login_form()`")
-st.write(
-    "`login_form()` creates the below form and returns the `Supabase.client` instance that can then be used to perform downstream supabase operations"
-)
+def get_vectorstore(text_chunks):
+    # Fetch the OpenAI API key from st.secrets
+    openai_api_key = st.secrets["openai"]["api_key"]
+    
+    # Pass the API key to OpenAIEmbeddings
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
 
-client = st_login_form.login_form(user_tablename="demo_users")
 
-st.write(
-    "On authentication, `login_form()` sets the `st.session_state['authenticated']` to `True`. This also collapses and disables the login form."
-)
-st.write(
-    "`st.session_state['username']` is set to the provided username for a new or existing user, and to `None` for guest login."
-)
 
+def get_conversation_chain(vectorstore):
+    # Fetch the OpenAI API key from st.secrets
+    openai_api_key = st.secrets["openai"]["api_key"]
+
+    llm = ChatOpenAI(openai_api_key=openai_api_key)
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
+
+# Retrieve secrets from st.secrets
+openai_api_key = st.secrets["openai"]["api_key"]
+
+# Check if the user is authenticated
 if st.session_state["authenticated"]:
     if st.session_state["username"]:
         st.success(f"Welcome {st.session_state['username']}")
+
+        main()  # Call the main function for the chat interface
+
     else:
         st.success("Welcome guest")
 else:
     st.error("Not authenticated")
+
